a39c730060a5a4b117cb2f7240327af137ae1173
Granit Berisha <b3y0utub3@gmail.com>
2019-09-27 17:10:58 +0200

    Delete a color from ColorFactory

--- app/src/main/java/com/tickks/ColorFactory.java
+++ app/src/main/java/com/tickks/ColorFactory.java
@@ -6,7 +6,8 @@ public class ColorFactory {
 
   String[] colors = {"#39add1", "#3079ab", "#c25975",
       "#e15258", "#f9845b", "#838cc7", "#7d669e", "#53bbb4", "#51b46d", "#e0ab18",
-      "#637a91", "#f092b0", "#b7c0c7"};
+      "#637a91", "#f092b0"
+      };
 
 
   public String getColor() {
