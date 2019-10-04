d1dd170e656a0ce2783bc9bc67ec0c12b86c0a15
Granit Berisha <b3y0utub3@gmail.com>
2019-10-04 17:12:41 +0200

    Add Fragment

--- app/src/main/java/com/tickks/AddFact.java
+++ app/src/main/java/com/tickks/AddFact.java
@@ -3,11 +3,15 @@ package com.tickks;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 
 /**
@@ -28,4 +32,13 @@ public class AddFact extends Fragment {
     return inflater.inflate(R.layout.fragment_add_fact, container, false);
   }
 
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+
+    EditText addFactText = view.findViewById(R.id.add_text);
+    Button cancelButton = view.findViewById(R.id.cancel_button);
+    Button addButton = view.findViewById(R.id.add_button);
+
+  }
 }
